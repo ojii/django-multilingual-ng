439bb466465671014bca4225e65d085fce1ec8b3
marcin.kaszynski <marcin.kaszynski@7f4da543-ba27-0410-bf1f-6504834a333d>
2007-02-04 12:32:44 +0000

    Changed set_default_language and get_default_language to use thread local storage instead of a global variable.  This fixes #9.
    git-svn-id: http://django-multilingual.googlecode.com/svn/trunk@27 7f4da543-ba27-0410-bf1f-6504834a333d

--- multilingual/languages.py
+++ multilingual/languages.py
@@ -6,17 +6,24 @@ Django-multilingual: language-related settings and functions.
 from django.conf import settings
 LANGUAGES = settings.LANGUAGES
 
+try:
+    from threading import local
+except ImportError:
+    from django.utils._threading_local import local
+
+thread_locals = local()
+
 #dev-note: it might be nice to define the default language per-model
-DEFAULT_LANGUAGE = settings.DEFAULT_LANGUAGE
+thread_locals.DEFAULT_LANGUAGE = settings.DEFAULT_LANGUAGE
 
 def get_language_count():
     return len(LANGUAGES)
 
 def get_language_code(language_id):
-    return LANGUAGES[(int(language_id or DEFAULT_LANGUAGE)) - 1][0]
+    return LANGUAGES[(int(language_id or thread_locals.DEFAULT_LANGUAGE)) - 1][0]
 
 def get_language_name(language_id):
-    return LANGUAGES[(int(language_id or DEFAULT_LANGUAGE)) - 1][1]
+    return LANGUAGES[(int(language_id or thread_locals.DEFAULT_LANGUAGE)) - 1][1]
 
 def get_language_id_list():
     return range(1, get_language_count() + 1)
@@ -45,14 +52,13 @@ def set_default_language(language_id_or_code):
 
     To do: find a better place to store the value.
     """
-    import multilingual.languages
     language_id = get_language_id_from_id_or_code(language_id_or_code)
-    multilingual.languages.DEFAULT_LANGUAGE = language_id
+    thread_locals.DEFAULT_LANGUAGE = language_id
 
 def get_default_language():
     # you might take the ID from elsewhere, ie
     # cookies or threadlocals
-    return DEFAULT_LANGUAGE
+    return thread_locals.DEFAULT_LANGUAGE
 
 def get_translation_table_alias(translation_table_name, language_id):
     """
