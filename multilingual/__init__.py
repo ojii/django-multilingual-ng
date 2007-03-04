2802f8369f83f5987238a79a6ec5769ce163b984
marcin.kaszynski <marcin.kaszynski@7f4da543-ba27-0410-bf1f-6504834a333d>
2007-03-04 21:33:16 +0000

    Made get_language_code_list accessible via top-level multilingual module.
    git-svn-id: http://django-multilingual.googlecode.com/svn/trunk@48 7f4da543-ba27-0410-bf1f-6504834a333d

--- multilingual/__init__.py
+++ multilingual/__init__.py
@@ -4,6 +4,6 @@ Django-multilingual: multilingual model support for Django.
 
 import models
 from exceptions import TranslationDoesNotExist, LanguageDoesNotExist
-from languages import set_default_language
+from languages import set_default_language, get_default_language, get_language_code_list
 from translation import Translation
 from manager import Manager
