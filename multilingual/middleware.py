3fbe3d88572f3ad507ea5763ea281a890a2e271d
joost@cassee.net <joost@cassee.net@7f4da543-ba27-0410-bf1f-6504834a333d>
2008-06-09 14:53:52 +0000

    Added middleware to set default language from get_language.
    git-svn-id: http://django-multilingual.googlecode.com/svn/trunk@97 7f4da543-ba27-0410-bf1f-6504834a333d

---
+++ multilingual/middleware.py
@@ -0,0 +1,18 @@
+from django.utils.translation import get_language
+from multilingual.exceptions import LanguageDoesNotExist
+from multilingual.languages import set_default_language
+
+class DefaultLanguageMiddleware(object):
+    """
+    Binds DEFAULT_LANGUAGE_CODE to django's currently selected language.
+
+    The effect of enabling this middleware is that translated fields can be
+    accessed by their name; i.e. model.field instead of model.field_en.
+    """
+    def process_request(self, request):
+        assert hasattr(request, 'session'), "The DefaultLanguageMiddleware middleware requires session middleware to be installed. Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.sessions.middleware.SessionMiddleware'."
+        try:
+            set_default_language(get_language())
+        except LanguageDoesNotExist:
+            # Try without the territory suffix
+            set_default_language(get_language()[:2])
