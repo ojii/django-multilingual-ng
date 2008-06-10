1c00fbea9cfe8dfc0e985f1c1188a3b3991c6954
joost@cassee.net <joost@cassee.net@7f4da543-ba27-0410-bf1f-6504834a333d>
2008-06-10 01:34:59 +0000

    Changed LANGUAGE_CODE from 'en-us' to 'en'. Added flatpages to the test set-up.
    git-svn-id: http://django-multilingual.googlecode.com/svn/trunk@104 7f4da543-ba27-0410-bf1f-6504834a333d

--- testproject/settings.py
+++ testproject/settings.py
@@ -23,7 +23,7 @@ TIME_ZONE = 'America/Chicago'
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 # django-multilanguange #####################
 
@@ -74,6 +74,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
+    'multilingual.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'testproject.urls'
@@ -93,6 +94,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'multilingual',
+    'multilingual.flatpages',
     'testproject.articles',
     'testproject.issue_15',
     'testproject.issue_16',
