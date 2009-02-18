739a7ae47a0a4352703ea1ca7282155f9188200e
marcin.kaszynski <marcin.kaszynski@7f4da543-ba27-0410-bf1f-6504834a333d>
2009-02-18 23:52:57 +0000

    Added a simple setup.py script, as suggested in issue #91. Thanks, JustinLilly!
    git-svn-id: http://django-multilingual.googlecode.com/svn/trunk@158 7f4da543-ba27-0410-bf1f-6504834a333d

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'django-multilingual',
+    description = 'Multilingual extension for Django',
+    author = 'Marcin Kaszynski',
+    url = 'http://code.google.com/p/django-multilingual/',
+    packages = ['multilingual'],
+)
