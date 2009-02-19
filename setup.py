cf6e40133bde8999edc487edf838ddbc1f595293
marcin.kaszynski <marcin.kaszynski@7f4da543-ba27-0410-bf1f-6504834a333d>
2009-02-19 01:15:28 +0000

    Added the templates and fixtures to setup.py. Thanks, Jannis Leidel!
    git-svn-id: http://django-multilingual.googlecode.com/svn/trunk@159 7f4da543-ba27-0410-bf1f-6504834a333d

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'django-multilingual',
+    version = '0.1.0',
     description = 'Multilingual extension for Django',
     author = 'Marcin Kaszynski',
     url = 'http://code.google.com/p/django-multilingual/',
-    packages = ['multilingual'],
+    packages = find_packages(exclude=["testproject", "testproject.*"]),
+    zip_safe=False,
+    package_data = {
+        '': ['templates/*/*.html'],
+    },
 )
