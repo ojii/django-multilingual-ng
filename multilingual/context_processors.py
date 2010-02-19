42b26e239029d93ae245aa0f03e3dc0b80027480
ojii <jonas.obrist@divio.ch>
2010-02-19 16:13:42 +0100

    improved the admin UI even more

--- multilingual/context_processors.py
+++ multilingual/context_processors.py
@@ -1,11 +1,14 @@
-from multilingual.languages import (
-    get_language_code_list,
-    get_default_language_code)
+from multilingual.languages import get_language_code_list, get_default_language_code
+from multilingual.settings import LANG_DICT
+from django.conf import settings
 
 
 def multilingual(request):
     """
     Returns context variables containing information about available languages.
     """
-    return {'LANGUAGE_CODES': get_language_code_list(),
-            'DEFAULT_LANGUAGE_CODE': get_default_language_code()}
+    codes = sorted(get_language_code_list())
+    return {'LANGUAGE_CODES': codes,
+            'LANGUAGE_CODES_AND_NAMES': [(c, LANG_DICT.get(c, c)) for c in codes], 
+            'DEFAULT_LANGUAGE_CODE': get_default_language_code(),
+            'ADMIN_MEDIA_URL': settings.ADMIN_MEDIA_PREFIX}
