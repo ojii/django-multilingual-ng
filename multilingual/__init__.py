fa6d82d32ea534c96aaf6de3b1936386df51a8f0
ojii <jonas.obrist@divio.ch>
2010-02-19 16:14:12 +0100

    bumped version number

--- multilingual/__init__.py
+++ multilingual/__init__.py
@@ -3,8 +3,8 @@ Django-multilingual-ng: multilingual model support for Django 1.2.
 """
 import warnings
 
-VERSION = (0,1,0,'b2')
-__version__ = '0.1.0b2'
+VERSION = (0,1,0,'b3')
+__version__ = '0.1.0b3'
 
 try:
     from multilingual import models
